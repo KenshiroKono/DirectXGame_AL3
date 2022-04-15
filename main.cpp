58b5f99450c0b768364bdc19115321798cb00749
KenshiroKono <k021g1265@g.neec.ac.jp>
2022-04-15 11:57:51 +0900

    tst

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow("LE2C_12_コウノ_ケンシロウ");
+	win->CreateGameWindow("LE2C_12_コウノ_ケンシロウ0102");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
